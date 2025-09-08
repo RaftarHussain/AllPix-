ef03b07ecd77c1053387bbdcf995ee5d5222a729
raftar <mewraftar7@gmail.com>
2025-09-08 12:34:08 +0530

    database error fix

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -92,11 +92,7 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
-    DATABASES["default"] = dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=True
-    )
+    DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
 
 
 # Password validation
